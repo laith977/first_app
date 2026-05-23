aa57c8840981c456da006dfc8329c9a5293dff68
Laith Adnan <lith.adnan@yahoo.com>
2026-05-23 16:53:06 +0300

    small optimization

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,28 +1,22 @@
 import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 Alignment startAlignment = Alignment.topLeft;
 Alignment endAlignment = Alignment.bottomRight;
 
 // color array instead of two colors
 class GradientContainer extends StatelessWidget {
-  GradientContainer({
+  const GradientContainer({
     super.key,
     required this.colors,
   });
-  GradientContainer.purple({super.key})
+  const GradientContainer.purple({super.key})
     : colors = const [
         Color.fromARGB(255, 63, 5, 120),
         Color.fromARGB(255, 100, 5, 120),
       ];
 
   final List<Color> colors;
-  var activeDiceImage = 'assets/images/dice-1.png';
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-    print(activeDiceImage);
-  }
 
   @override
   Widget build(BuildContext context) {
